101f5d1beafd05ab51aa434c5971a48dc9014e05
Yen Colon <ycolon@silta.com>
2022-05-14 20:02:28 -0400

    Vista principal con componentes unidos

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Nav from './components/Nav';
+import Banner from './components/Banner';
+import { BeCountedAlert, Footer, SubmitName } from './components/Basics';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Nav />
+      <Banner />
+      <div className="max-centered">
+        <BeCountedAlert />
+        <main role="main">
+          👉 Your code goes here 👈
+        </main>
+        <SubmitName />
+        <Footer />
+      </div>
+    </>
   );
 }
 
