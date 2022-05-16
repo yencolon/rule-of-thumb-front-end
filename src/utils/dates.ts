0b5639f2e2c3c5161008e42b2ba7113275a23aea
Yen Colon <ycolon@silta.com>
2022-05-15 23:34:27 -0400

    Cambios para evitar warning  en justify-content

--- src/utils/dates.ts
+++ src/utils/dates.ts
@@ -1,7 +1,7 @@
 /**
- * Funcion que recibe un fecha en formato string 
- * y devuelve la cantidad de tiempo que ha pasado desde entonces
- * @param date string, en formato ISO YYYY/MM/DD HH:MM:SS
+ * Function that receives a date in string format
+ * and returns the amount of time that has passed since then
+ * @param date string, ISO YYYY/MM/DD HH:MM:SS
  */
 export const calculateHowMuchTimePastFromNow = (date: string): string => {
     const initialDate = new Date(date).getTime()
@@ -11,10 +11,9 @@ export const calculateHowMuchTimePastFromNow = (date: string): string => {
 }
 
 /**
- * Función simple, que dada la resta de dos fechas, devuelve el  tiempo relativo que ha
- * transcurrido en un formato [cantidad] [unidad]
- * @param timestamp number, fechaInicial - fechaFinal
- * @returns string con el tiempo que ha pasado
+ * Function, given the subtraction of two dates, returns the relative time that has
+ * elapsed in a format [quantity] [unit] format
+ * @param timestamp number, startDate - finalState
  */
 const getHumanTime = (timestamp: number): string => {
     // Convert to a positive integer
