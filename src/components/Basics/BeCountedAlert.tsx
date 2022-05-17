c21f3d4f15254f2d7b4be6f48818d51a4b38a940
Yen Colon <ycolon@silta.com>
2022-05-16 20:50:37 -0400

    Solucion errores al pasar hmtl a JSX

--- src/components/Basics/BeCountedAlert.tsx
+++ src/components/Basics/BeCountedAlert.tsx
@@ -13,7 +13,7 @@ const BeCountedAlert = () => {
                 </p>
             </div>
             <button className="icon-button" aria-label="close">
-                <svg width="20" height="20" xmlns="http://www.w3.org/2000/svg"><g stroke="#000" stroke-width="2" fill="none" fill-rule="evenodd"><path d="M1 19L19 1M1 1l18 18" /></g></svg>
+                <svg width="20" height="20" xmlns="http://www.w3.org/2000/svg"><g stroke="#000" strokeWidth="2" fill="none" fillRule="evenodd"><path d="M1 19L19 1M1 1l18 18" /></g></svg>
             </button>
         </aside>
     )
